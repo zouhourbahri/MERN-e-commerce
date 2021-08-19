515900eaa9aa396eeaba2fc9d24d310558300f53
zouhour bahri <zouhour.bahri@yahoo.com>
2021-08-19 13:46:24 +0200

    uptodate

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,11 @@ const connectDB = require('./config/connectDB');
 const app = express();
 app.use(express.json());
 
-// connect to DB
-connectDB.connectDB();
+connectDB.connectDB(); // connect to DB
 
 // routes
 app.use('/TUNISHOP', user);
-// app.use('/', profil);
 
 // server
 const PORT = process.env.PORT || 7000;
-
 app.listen(PORT, (error) => error ? console.log(error) : console.log(`Server is running on localhost: ${PORT}`));
